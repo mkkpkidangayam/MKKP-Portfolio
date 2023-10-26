d3729d055c58a946287225d18e57a4146f480d38
mkkpkidangayam <mkkpkdm@gmail.com>
2023-10-27 00:06:07 +0530

    Updated on 27/10/2023

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,32 +1,38 @@
-function sendMail(event){
+function sendMail(event) {
     event.preventDefault();
 
+    // Get input values
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
 
-var params = {
-    name: document.getElementById("name").value ,
-    email: document.getElementById("email").value ,
-    subject: document.getElementById("subject").value ,
-    message: document.getElementById("message").value ,
-}
-
-
-const serviceID = "service_dbch618";
-const templateID = "template_w12z4kx";
-
-emailjs.send(serviceID, templateID, params)
-.then(   
-res =>{
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("subject").value = "";
-    document.getElementById("message").value = "";
-    console.log(res);
-    alert("Your message sent successfully")
+    // Check if required fields are filled
+    if (!name || !email || !subject || !message) {
+        alert("Please fill in all the required fields.");
+        return; // Stop the function if fields are missing
     }
-)
 
-.catch((err) => console.log(err));
+    // Prepare the email parameters
+    var params = {
+        name: name,
+        email: email,
+        subject: subject,
+        message: message
+    };
+
+    const serviceID = "service_dbch618";
+    const templateID = "template_w12z4kx";
+
+    emailjs.send(serviceID, templateID, params)
+        .then(res => {
+            // Clear the input fields
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("subject").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("Your message was sent successfully. Thank you!");
+        })
+        .catch(err => console.log(err));
 }
-
-
-
